a12596f7ed5693178def1123b9a9eeaa62096a43
Hampus von Horn <hampus.vh@gmail.com>
2025-08-09 13:10:08 +0200

    more styling and structural changes to chat/sidenav

--- src/pages/Chat.jsx
+++ src/pages/Chat.jsx
@@ -3,18 +3,23 @@ import "../styles/Chat.css";
 
 const Chat = () => {
     return (
-        <div className="chat-page">
-            <SideNav />
-            <main className="chat-content">
-                <div className="message-output">
-                    Message output
-                </div>
-                <div className="composer">
-                    <input className="message-input"
-                        type="text"
-                        placeholder="write your message here"
-                    />
-                    <button className="send-btn" type="button">Send</button>
+        <div className="chat-layout">
+            <SideNav />               {/* aside i sin egen komponent */}
+
+            <main className="chat-main">
+                <div className="container">
+                    <div className="chat-body">
+                        Message output
+                    </div>
+
+                    <div className="chat-composer">
+                        <input
+                            className="composer-input"
+                            type="text"
+                            placeholder="write your message here"
+                        />
+                        <button className="composer-send" type="button">Send</button>
+                    </div>
                 </div>
             </main>
         </div>
