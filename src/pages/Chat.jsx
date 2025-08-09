e0b58e1b38d512ce835667d088fae619fa29c8b2
Hampus von Horn <hampus.vh@gmail.com>
2025-08-09 15:36:34 +0200

    fixed structure and removed unnecessary wrapper. added more styling

--- src/pages/Chat.jsx
+++ src/pages/Chat.jsx
@@ -3,23 +3,21 @@ import "../styles/Chat.css";
 
 const Chat = () => {
     return (
-        <div className="chat-layout">
-            <SideNav />               {/* aside i sin egen komponent */}
+        <div className="chat-wrapper">
+            <SideNav />
 
-            <main className="chat-main">
-                <div className="container">
-                    <div className="chat-body">
-                        Message output
-                    </div>
+            <main className="chat-content">
+                <div className="chat-output">
+                    Message output
+                </div>
 
-                    <div className="chat-composer">
-                        <input
-                            className="composer-input"
-                            type="text"
-                            placeholder="write your message here"
-                        />
-                        <button className="composer-send" type="button">Send</button>
-                    </div>
+                <div className="chat-composer">
+                    <input
+                        className="composer-input"
+                        type="text"
+                        placeholder="write your message here"
+                    />
+                    <button className="send-button" type="button">Send</button>
                 </div>
             </main>
         </div>
