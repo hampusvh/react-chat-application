a657d94ddd119768ae920ff7b3e1f4c6ae5d29b4
Hampus von Horn <hampus.vh@gmail.com>
2025-08-16 12:34:53 +0200

    Sentry implemented in application

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,12 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./styles/Global.css";
 
+import * as Sentry from "@sentry/react";
+
+Sentry.init({
+  dsn: "https://b81470fe7f262afd85de1fa398028a4c@o4509853252255744.ingest.de.sentry.io/4509853258678352",
+});
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
